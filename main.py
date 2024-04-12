831132da147af5d09c7a0964f8284ac3c1bbde13
William Richard Mangoni <willrpg@hotmail.com>
2024-04-12 16:49:23 -0300

    Ajustes de parser nas strings pra remover acentos e caracteres especiais

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
 import os
+from unidecode import unidecode
+import re
+
+
+def remover_caracteres_especiais(palavra):
+    # Substituir todos os caracteres que não são letras ou números por uma string vazia
+    palavra_sem_especiais = re.sub(r'[^a-zA-Z0-9]', '', palavra)
+    return palavra_sem_especiais
+
 
 def scrape_letras_musicais(url_base):
     # Faz uma solicitação GET para a URL base
@@ -22,6 +31,7 @@ def scrape_letras_musicais(url_base):
         for link_artista in links_artistas:
             # Extrai o atributo href de cada tag <a>
             href = link_artista['href']
+            href_formated = unidecode(remover_caracteres_especiais(href.replace("/", "")))
 
             response_artista = requests.get(url_base + href)
             if response_artista.status_code == 200:
@@ -33,27 +43,28 @@ def scrape_letras_musicais(url_base):
                 print(songs[0])
 
                 for song in songs:
-                    print(song)
+                    print(song.encode('utf-8'))
 
                     resp_song = requests.get(url_base + song['href'])
 
                     soup_song = BeautifulSoup(resp_song.text, 'html.parser')
 
                     lyric = soup_song.find('div', class_='lyric-original')
-                    print(lyric.text.strip())
+                    print(lyric.text.strip().encode('utf-8'))
 
                     #Se não existir o diretório, temos que criar um
-                    if not os.path.exists(f'conteudo_artistas/{href}'):
-                        os.makedirs(f'conteudo_artistas/{href}')
+                    if not os.path.exists(f'conteudo_artistas/{href_formated}'):
+                        os.makedirs(f'conteudo_artistas/{href_formated}')
 
                     # Salva o conteúdo em um arquivo separado
                     s = song['title'].replace("/", "_")
-                    nome_arquivo = f'conteudo_artistas/{href}/{s}.txt'
+                    s_formated = unidecode(remover_caracteres_especiais(s.replace(" ", "_")))
+                    nome_arquivo = f'conteudo_artistas/{href_formated}/{s_formated}.txt'
 
                     with open(nome_arquivo, 'w', encoding='utf-8') as file:
                         file.write(lyric.text.strip())
 
-                    print(f"Conteúdo do artista {href} salvo com sucesso em {nome_arquivo}.")
+                    print(f"Conteúdo do artista {href.encode('utf-8')} salvo com sucesso em {nome_arquivo.encode('utf-8')}.")
             else:
                 print(f"Erro ao fazer a requisição HTTP para {url_completa}.")
     else:
