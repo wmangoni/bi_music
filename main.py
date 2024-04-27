a511426b8ff06d7771a178038dc30b54bf3a1660
William <willrpg@hotmail.com>
2024-04-27 13:18:01 -0300

    criação de api de consulta

--- main.py
+++ main.py
@@ -1,181 +1,64 @@
-import requests
-from bs4 import BeautifulSoup
-import os
-from unidecode import unidecode
-import re
-import string
+from typing import Union
 
-def remover_caracteres_especiais(palavra):
-    # Substituir todos os caracteres que não são letras ou números por uma string vazia
-    palavra_sem_especiais = re.sub(r'[^a-zA-Z0-9]', '', palavra)
-    return palavra_sem_especiais
+from fastapi import FastAPI, Query
 
-def extrair_nome_arquivo(song_title, href_formated):
-    # Formata o nome do arquivo removendo caracteres especiais e substituindo espaços por underscores
-    s_formated = unidecode(remover_caracteres_especiais(song_title.replace(" ", "_")))
-    return f'{DIRRETORIO_PRINCIPAL}/{href_formated}/{s_formated}.txt'
+import pandas as pd
 
-def salvar_cifra(nome_arquivo, cifra, title_song):
-    # Salva o conteúdo da cifra em um arquivo
-    with open(nome_arquivo.replace('.txt', '_cifra.html'), 'w', encoding='utf-8') as file:
-        html = f'<html><h1>{title_song.get_text() + "<h1><div>" + str(cifra.pre)}</div></html>'
-        file.write(html)
+app = FastAPI()
 
-def salvar_lyric(nome_arquivo, lyric_content, title_song):
-    # Garante que o diretório existe
-    os.makedirs(os.path.dirname(nome_arquivo), exist_ok=True)
+# Caminho para o arquivo CSV
+caminho_arquivo = 'bi_music/bi_music.csv'
 
-    # Salva o conteúdo da letra em um arquivo
-    with open(nome_arquivo, 'w', encoding='utf-8') as file:
-        for l in lyric_content:
-            if MODO_TESTE:
-                print(str(l.contents))
-            all_content = title_song.get_text() + "\n \n" + str(l.contents) \
-                .replace("]", "\n") \
-                .replace("['", "") \
-                .replace("[\"", "") \
-                .replace(" ,", "") \
-                .replace("\",", "") \
-                .replace("',", " ") \
-                .replace(" '", " ") \
-                .replace("\"'", "") \
-                .replace("\"", "") \
-                .replace(" <br/>, ", "\n")
-            file.write(all_content.replace("'\n", "\n"))
+# Ler o arquivo CSV usando o pandas
+df = pd.read_csv(caminho_arquivo, encoding='utf-8')
 
-def scrape_letras_musicais(url_base):
+df = df.drop_duplicates(subset='Lyrics')
+df = df.dropna()
+df = df.reset_index(drop=True)
 
-    # Cria um array com todas as letras do alfabeto
-    letras_alfabeto = list(string.ascii_uppercase)
-    
-    # se quiser executar apenas algumas letras pode fazer assim:
-    # letras_alfabeto = ['R', 'S', 'T', 'U', 'V', 'X', 'Y', 'W', 'Z']
+def extract_song_name(lyrics_text):
+  if lyrics_text:
+      return lyrics_text.split("\n")[0]  # Retorna o nome da música se 'Lyrics' não for vazia
+  else:                                   
+      return "Música sem nome"            # Valor padrão se 'Lyrics' for vazia
 
-    for letra in letras_alfabeto:
+df["SongName"] = df["Lyrics"].apply(extract_song_name) 
+df['comprimento_letra'] = df['Lyrics'].str.len()
+df['ocorrencias_amor_love'] = df['Lyrics'].str.count('amor|love')
 
-        response = requests.get(url_base + "/letra/" + letra.upper() + "/") ### EXEMPLO: https://www.letras.mus.br/letra/K/
-        if response.status_code == 200:
-            soup = BeautifulSoup(response.text, 'html.parser')
-            links_artistas = soup.find(CLASSE_LINK_ARTISTAS[0], class_=CLASSE_LINK_ARTISTAS[1]).find_all('a')
+# Visualizar as primeiras linhas do DataFrame para garantir que os dados foram carregados corretamente
 
-            # Garante que o diretório existe
-            os.makedirs(DIRRETORIO_PRINCIPAL, exist_ok=True)
+def paginateDf(page, per_page, colunm, name):
+    start_index = (page - 1) * per_page
+    end_index = start_index + per_page
 
-            for link_artista in links_artistas:
-                href = link_artista['href']
-                href_formated = unidecode(remover_caracteres_especiais(href.replace("/", "")))
-                music_classification = link_artista.find('small')
-                if music_classification is None:
-                    music_classification = ""
-                else:
-                    music_classification = music_classification.get_text()
+    df_page = df.iloc[start_index:end_index]
 
-                scrape_musica_artista(url_base + href, href_formated, music_classification)
-                if MODO_TESTE:
-                    break
-        else:
-            print("Erro ao fazer a requisição HTTP para a URL base.")
-
-        if MODO_TESTE:
-            break
-
-def scrape_musica_artista(url_artista, href_formated, music_classification):
-    response_artista = requests.get(url_artista) ### EXEMPLO: https://www.letras.mus.br/king-king/
-    if response_artista.status_code == 200:
-        soup_artista = BeautifulSoup(response_artista.text, 'html.parser')
-
-        name_artistic = soup_artista.find(CLASSE_TITLE[0], class_=CLASSE_TITLE[1])
-        file_name = f'{DIRRETORIO_PRINCIPAL}/{href_formated}/metadata.txt'
-        os.makedirs(os.path.dirname(file_name), exist_ok=True)
-        with open(file_name, 'w', encoding='utf-8') as file:
-            file.write(name_artistic.get_text() + "\n" + music_classification)
-
-        songs = soup_artista.find(CLASSE_CONTEUDO[0], class_=CLASSE_CONTEUDO[1]).find_all('a')
-        
-        for song in songs:
-            
-            print(song['href'].encode('utf-8'))
-            resp_song = requests.get(url_base + song['href']) ### EXEMPLO: https://www.letras.mus.br/king-king/you-stopped-the-rain/
-
-            #resp_song_cifra = requests.get(url_base_cifra + song['href']) ### EXEMPLO: https://www.cifraclub.com.br/king-king/you-stopped-the-rain/
-            
-            if resp_song.status_code == 200:
-                
-                soup_song = BeautifulSoup(resp_song.text, 'html.parser')
-
-                title_song = soup_song.find(CLASSE_TITLE[0], class_=CLASSE_TITLE[1])
-
-                list = soup_song.find('div', class_='lyric-menu').find_all('a')
-
-                for i in list:
-                    if 'data-action' in str(i):
-                        if  i['data-action'] == 'Nav Cifra':
-                            href_cifra = i['href']
-                            resp_song_cifra = requests.get(href_cifra) ### EXEMPLO: https://www.cifraclub.com.br/king-king/you-stopped-the-rain/
-                            handle_cifra(resp_song_cifra, song, href_formated, title_song)
-                
-                lyric = soup_song.find(CLASSE_LYRIC[0], class_=CLASSE_LYRIC[1]).find_all("p")
-                
-                if lyric:
-                    nome_arquivo = extrair_nome_arquivo(song['title'], href_formated)
-                    salvar_lyric(nome_arquivo, lyric, title_song)
-                else:
-                    print(f"Letra não encontrada para a música: {song['title']}")
-                    file_name = f'{DIRRETORIO_PRINCIPAL}/{href_formated}/metadata.txt'
-                    with open(file_name, 'a', encoding='utf-8') as file:
-                        file.write("\n" + f"Letra não encontrada para a música: {song['title']}")
-            else:
-                print(f"Erro ao fazer a requisição HTTP para {url_base + song['href']}.")
-                file_name = f'{DIRRETORIO_PRINCIPAL}/{href_formated}/metadata.txt'
-                with open(file_name, 'a', encoding='utf-8') as file:
-                    file.write("\n" + f"Erro ao fazer a requisição HTTP para {url_base + song['href']}.")
-
-            
-
-            if MODO_TESTE:
-                break
-    else:
-        print(f"Erro ao fazer a requisição HTTP para {url_artista}.")
+    df_filtrado = df_page[df[colunm] == name]
+    return df_filtrado.to_json(orient='records')
 
 
-def handle_cifra(resp_song_cifra, song, href_formated, title_song):
-    if resp_song_cifra.status_code == 200:
-                
-        resp_song_cifra = BeautifulSoup(resp_song_cifra.text, 'html.parser')
+@app.get("/songs")
+def get_all(page: int = Query(default=1, ge=1), per_page: int = Query(default=10, gt=0)):
 
-        cifra = resp_song_cifra.find('div', class_='cifra_cnt g-fix cifra-mono')
-        
-        if cifra:
-            nome_arquivo = extrair_nome_arquivo(song['title'], href_formated)
-            salvar_cifra(nome_arquivo, cifra, title_song)
-        else:
-            print(f"Cifra não encontrada para a música: {song['title']}")
-            file_name = f'{DIRRETORIO_PRINCIPAL}/{href_formated}/metadata.txt'
-            with open(file_name, 'a', encoding='utf-8') as file:
-                file.write("\n" + f"Cifra não encontrada para a música: {song['title']}")
-    else:
-        print(f"Erro ao fazer a requisição HTTP para {url_base_cifra + song['href']}.")
-        file_name = f'{DIRRETORIO_PRINCIPAL}/{href_formated}/metadata.txt'
-        with open(file_name, 'a', encoding='utf-8') as file:
-            file.write("\n" + f"Erro ao fazer a requisição HTTP para {url_base_cifra + song['href']}.")
+    start_index = (page - 1) * per_page
+    end_index = start_index + per_page
+
+    df_page = df.iloc[start_index:end_index]
+
+    df_paginado_filtrado = df_page.iloc[:, [0, 1, 4, 5, 6]]
+
+    return df_paginado_filtrado.to_json(orient='records')
 
 
-MODO_TESTE = False
-DIRRETORIO_PRINCIPAL = 'conteudo_artistas'
-CLASSE_LINK_ARTISTAS = ('ul', 'homeRecommendedArtists-artists')
-CLASSE_CONTEUDO = ('ol', 'artist-songList-content')
-CLASSE_TITLE = ('h1', 'head-title')
-CLASSE_LYRIC = ('div', 'lyric-original')
+@app.get("/songs/{name}")
+def get_songs(name: str, page: int = Query(default=1, ge=1), per_page: int = Query(default=10, gt=0)):
+    return paginateDf(page, per_page, "SongName", name)
 
+@app.get("/genre/{name}")
+def get_genre(name: str, page: int = Query(default=1, ge=1), per_page: int = Query(default=10, gt=0)):
+    return paginateDf(page, per_page, "Genre", name)
 
-# URL base da página
-url_base = 'https://www.letras.mus.br'
-url_base_cifra = 'https://www.cifraclub.com.br'
-
-# Chama a função de scraping
-scrape_letras_musicais(url_base)
-
-#aqui pe pra rodar só um artista
-#scrape_musica_artista('https://www.letras.mus.br/legiao-urbana/', 'legiao-urbana', 'Pop Rock')
-
-## Ideia de raspagem futura: https://www.palcomp3.com.br/mp3/A/todos.htm
+@app.get("/artist/{name}")
+def read_item(name: str, page: int = Query(default=1, ge=1), per_page: int = Query(default=10, gt=0)):
+    return paginateDf(page, per_page, "Artist", name)
